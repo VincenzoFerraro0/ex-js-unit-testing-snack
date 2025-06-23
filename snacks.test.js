982f568b0fbefd4d30f00908023b250cc5d4af02
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-06-23 17:55:31 +0200

    Snack 1 completed

---
+++ snacks.test.js
@@ -0,0 +1,8 @@
+const { getInitials } = require("./snacks.js")
+
+// Snack 1
+test("La funzione getInitials restituisce le iniziali di un nome completo.", () => {
+    expect(getInitials("Mario Rossi")).toBe("M.R.")
+    expect(getInitials("luigi bianchi")).toBe("L.B.")
+    expect(getInitials("Maria  Verdi")).toBe("M.V.")
+})
