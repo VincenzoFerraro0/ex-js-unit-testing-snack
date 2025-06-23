68bdb251480117a24b88fa3442f5aaf7132d088e
VincenzoFerraro0 <ferrarov0@gmail.com>
2025-06-23 18:15:56 +0200

    snack 2 completed

--- snacks.js
+++ snacks.js
@@ -3,8 +3,15 @@
 function getInitials(nomeCompleto){
     const [nome, cognome] = nomeCompleto.split(" ").filter(str => str !== '');
     return `${nome.charAt(0).toUpperCase()}.${cognome.charAt(0).toUpperCase()}.`
-} 
+}
+
+// Snack 2
+function createSlug(str){
+    return str.toLowerCase()
+}
+
 
 module.exports = {
-    getInitials
+    getInitials,
+    createSlug
 }
--- snacks.test.js
+++ snacks.test.js
@@ -1,8 +1,15 @@
-const { getInitials } = require("./snacks.js")
+const { getInitials,createSlug  } = require("./snacks.js")
 
 // Snack 1
 test("La funzione getInitials restituisce le iniziali di un nome completo.", () => {
     expect(getInitials("Mario Rossi")).toBe("M.R.")
     expect(getInitials("luigi bianchi")).toBe("L.B.")
     expect(getInitials("Maria  Verdi")).toBe("M.V.")
+})
+
+
+
+// Snack 2
+test("La funzione createSlug restituisce una stringa in lowercase", () => {
+    expect(createSlug("CIAO")).toBe("ciao")
 })
